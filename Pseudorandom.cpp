a6b487bdcc4eacdc7b22cf1f1743f9a2b5a22716
Tuniphn <phantuanthanh2006@gmail.com>
2026-08-31 19:40:46 -0700

    Update Pseudorandom.cpp

--- Pseudorandom.cpp
+++ Pseudorandom.cpp
@@ -1,28 +1,106 @@
-#pragma once
-class Pseudorandom
+#include "Pseudorandom.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+Pseudorandom::Pseudorandom()
 {
-private:
-	int seed;
-	int multiplier;
-	int modulus;
-	int increment;
-public:
-	Pseudorandom();
+	seed = 1;
+	multiplier = 40;
+	modulus = 729;
+	increment = 725;
+}
 
-	int getSeed() const;
-	void setSeed(int newSeed);
+int Pseudorandom::getSeed() const
+{
+	return seed;
+}
+void Pseudorandom::setSeed(int newSeed)
+{
+	seed = newSeed;
+}
 
-	int getMultiplier() const;
-	void setMultiplier(int newMultiplier);
+int Pseudorandom::getMultiplier() const
+{
+	return multiplier;
+}
+void Pseudorandom::setMultiplier(int newMultiplier)
+{
+	multiplier = newMultiplier;
+}
 
-	int getModulus() const;
-	void setModulus(int newModulus);
+int Pseudorandom::getModulus() const
+{
+	return modulus;
+}
+void Pseudorandom::setModulus(int newModulus)
+{
+	modulus = newModulus;
+}
 
-	int getIncrement() const;
-	void setIncrement(int newIncrement);
+int Pseudorandom::getIncrement() const
+{
+	return increment;
+}
+void Pseudorandom::setIncrement(int newIncrement)
+{
+	increment = newIncrement;
+}
 
-	//next num, indirect next num, run experiment
-	int getNextNumber();
-	double getIndirectNextNumber();
-	void getExperimentResult();
-};
+int Pseudorandom::getNextNumber()
+{
+	int newNumber = (multiplier * seed + increment) % modulus;
+	seed = newNumber;
+	return newNumber;
+}
+
+double Pseudorandom::getIndirectNextNumber()
+{
+	return getNextNumber() / static_cast<double>(modulus);
+}
+
+void Pseudorandom::getExperimentResult()
+{
+	//random multiplier, increment, modulus  for experiment
+	multiplier = rand() % 100000 + 1;
+	increment = rand() % 100000 + 1;
+	modulus = rand() % 100000 + 1;
+
+	int interval = 0; // I want to turn the calculation of indirect next number to an integer to put in occurence
+	const int OCCURENCE = 10;
+	int occurenceCounter[OCCURENCE] = { 0 };
+
+	const int FUNCTIONCALLS = 1000000;
+	for (int i = 0; i < FUNCTIONCALLS; i++)
+	{
+		interval = getIndirectNextNumber() * 10;
+
+		occurenceCounter[interval]++;
+	}
+
+	cout << "\n\tExperiment of pseudorandom with random multiplier, increment and modulus:\n";
+	cout << "\n\t" << string(80, char(196));
+
+	cout << "\n\tMultiplier = " << multiplier << ", increment = " << increment << ", modulus = " << modulus << "\n";
+
+	cout << "\n\t" << setw(20) << left << "Range" << right << "Number of Occurrences";
+	for (int i = 0; i < OCCURENCE; i++)
+	{
+		if (i < 9)
+		{
+			cout << "\n\t[0." << i << " ... 0." << i + 1 << ")       " << occurenceCounter[i];
+		}
+		else
+		{
+			cout << "\n\t[0.9 ... 1.0)       " << occurenceCounter[i];
+		}
+	}
+	cout << "\n";
+
+	double sum = 0.0;
+
+	for (int i = 0; i < 12; i++)
+	{
+		sum += getIndirectNextNumber();
+	}
+}
