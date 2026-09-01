b1708ff9fad515fff4f111854d14ab064ee70fa4
Tuniphn <phantuanthanh2006@gmail.com>
2026-08-31 19:40:10 -0700

    Update Pseudorandom.cpp

--- Pseudorandom.cpp
+++ Pseudorandom.cpp
@@ -1,1 +1,28 @@
-#include "Pseudorandom.h"
+#pragma once
+class Pseudorandom
+{
+private:
+	int seed;
+	int multiplier;
+	int modulus;
+	int increment;
+public:
+	Pseudorandom();
+
+	int getSeed() const;
+	void setSeed(int newSeed);
+
+	int getMultiplier() const;
+	void setMultiplier(int newMultiplier);
+
+	int getModulus() const;
+	void setModulus(int newModulus);
+
+	int getIncrement() const;
+	void setIncrement(int newIncrement);
+
+	//next num, indirect next num, run experiment
+	int getNextNumber();
+	double getIndirectNextNumber();
+	void getExperimentResult();
+};
