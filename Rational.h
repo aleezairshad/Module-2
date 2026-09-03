425286a6e52c2bb3902e9dfeff2da1385eacd818
Aleeza Irshad <aleeza.irshad.20@gmail.com>
2026-09-02 17:37:00 -0700

    Add Rational

--- Rational.h
+++ Rational.h
@@ -1,5 +1,33 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Rational
 {
-};
+private:
+	int numerator;
+	int denominator;
 
+	int gcd(int a, int b) const;
+	void reduce();
+
+public:
+	Rational();
+	Rational(int n, int d);
+
+	void setNumerator(int n);
+	void setDenominator(int d);
+	int getNumerator() const;
+	int getDenominator() const;
+
+	Rational operator+(const Rational& rhs) const;
+	Rational operator-(const Rational& rhs) const;
+	Rational operator*(const Rational& rhs) const;
+	Rational operator/(const Rational& rhs) const;
+	bool operator==(const Rational& rhs) const;
+	bool operator<(const Rational& rhs) const;
+
+	string toString() const;
+	friend ostream& operator<<(ostream& out, const Rational& r);
+};
